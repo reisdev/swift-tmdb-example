0746011bf7a6a7bf2e96578471c417f349c4b60b
reisdev <matheusdrdj@gmail.com>
2022-06-11 18:59:50 -0300

    test[movie]: Adicionando teste para requisição de filmes populares

--- TMDBLibrary/Tests/TMDBServicesTests/MovieServiceTests.swift
+++ TMDBLibrary/Tests/TMDBServicesTests/MovieServiceTests.swift
@@ -12,14 +12,43 @@ final class MovieRequestTests: XCTestCase {
         MovieService.shared.getDetails(from: 406759)
             .subscribe(onNext: { movie in
                 
+                XCTAssertEqual(movie.title,"Moonfall")
             
-            XCTAssertEqual(movie.title,"Moonfall")
-            
-            expectation.fulfill()
+                expectation.fulfill()
             }, onError: { error in
                 XCTFail(error.localizedDescription)
             }).disposed(by: disposableBag)
 
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testGetPopularMovies() throws {
+        let expectation = expectation(description: "Can retrieve popular movies list")
+        
+        MovieService.shared.getPopular(page: 1)
+            .subscribe(onNext: { response in
+                XCTAssertGreaterThan(response.results.count, 0)
+                
+                expectation.fulfill()
+            }, onError: { error in
+                XCTFail(error.localizedDescription)
+            }).disposed(by: disposableBag)
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testGetPopularMoviesByPage() {
+        let expectation = expectation(description: "Can retrieve popular movies by page")
+        
+        MovieService.shared.getPopular(page: 2)
+            .subscribe(onNext: { response in
+                XCTAssertGreaterThan(response.results.count, 0)
+                XCTAssertEqual(response.page, 2)
+                expectation.fulfill()
+            }, onError: { error in
+                XCTFail(error.localizedDescription)
+            }).disposed(by: disposableBag)
+        
         wait(for: [expectation], timeout: 10.0)
     }
 }
